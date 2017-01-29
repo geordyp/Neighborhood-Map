1cc2a11e0786037e50d06e3b8278c366b3156388
geordyp <geowilliams6@gmail.com>
2017-01-28 22:36:36 -0600

    Clean up menu

--- js/app.js
+++ js/app.js
@@ -317,9 +317,13 @@ toggleBounce = function(rsrtClicked) {
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menu").style.left = "260px";
+    document.getElementById("menu").style.visibility = "hidden";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("menu").style.left = "10px";
+    document.getElementById("menu").style.visibility = "visible";
 }
