63bc5813a2ec9e50e57531fdb4b8d39020899264
geordyp <geowilliams6@gmail.com>
2017-01-28 20:27:03 -0600

    Hide side menu

--- js/app.js
+++ js/app.js
@@ -313,3 +313,13 @@ toggleBounce = function(rsrtClicked) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 };
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
